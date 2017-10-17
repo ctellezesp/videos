bd2dcab151570db1b96b342e92cc39c3672529bd
Carlos <A01274912@itesm.mx>
2017-10-17 17:50:57 -0500

    public added

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const port = 8080;
 
 const app = express();
 
-app.use('/', express.static(__dirname));
+app.use('/', express.static(__dirname+"/public"));
 
 app.get("/", function(req, res){
-	res.sendFile(process.cwd() + "/index.html");
+	res.sendFile(process.cwd() + "/public/index.html");
 })
 
 
